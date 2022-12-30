4025dacdbe9760e46e56e2eefe1b8489abefc24a
momo <naynara87@gmail.com>
2022-12-30 17:05:47 +0900

    Nicknames

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,30 +1,48 @@
-const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const messageList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
 
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 function handleOpen() {
-    console.log("Connected to Server ✅");
+  console.log('Connected to Server ✅');
 }
-socket.addEventListener("open", handleOpen);
+socket.addEventListener('open', handleOpen);
 
-socket.addEventListener("message", (message) => {
-    console.log("New message:", message.data)
+socket.addEventListener('message', (message) => {
+  // console.log("New message:", message.data)
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
-socket.addEventListener("close", () => {
-    console.log("connected to server x");
+socket.addEventListener('close', () => {
+  console.log('connected to server x');
 });
 
 setTimeout(() => {
-    socket.send("hello from thd browser")
+  socket.send('hello from thd browser');
 }, 10000);
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(input.value);
-    input.value = ""
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  //   socket.send(input.value);
+  input.value = '';
 }
-messageForm.addEventListener("submit", handleSubmit)
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,39 +1,39 @@
-import http from "http";
-import webSocket from "ws";
-import express from "express";
+import http from 'http';
+import webSocket from 'ws';
+import express from 'express';
+import { type } from 'os';
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.render("/"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.render('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 const wss = new webSocket.Server({ server });
 
-
 function onSocketClose() {
-    console.log("Disconnected from the Browser X")
+  console.log('Disconnected from the Browser X');
 }
 
 const sockets = [];
 
 function handleConnection(socket) {
-    sockets.push(socket);
-    console.log("connected to Brower ok!");
-    socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString()))
-        // socket.send(message.toString('ascii'));
-        // console.log(message.toString('ascii'));
-    });
-    socket.send("hello");
+  sockets.push(socket);
+  console.log('connected to Brower ok!');
+  socket.on('close', onSocketClose);
+  socket.on('message', (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    // socket.send(message.toString('ascii'));
+    // console.log(message.toString('ascii'));
+  });
+  socket.send('hello');
 }
 
-wss.on("connection", handleConnection);
+wss.on('connection', handleConnection);
 
-server.listen(3000, handleListen); 
+server.listen(3000, handleListen);
